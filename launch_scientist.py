8151e2df2445b1de6150134984880eee5202fb44
matchcase <shahsarthakw@gmail.com>
2025-08-14 23:59:13 +0530

    Fixed bug with --skip-novelty-check

--- launch_scientist.py
+++ launch_scientist.py
@@ -364,7 +364,7 @@ if __name__ == "__main__":
     with open(osp.join(base_dir, "ideas.json"), "w") as f:
         json.dump(ideas, f, indent=4)
 
-    novel_ideas = [idea for idea in ideas if idea["novel"]]
+    novel_ideas = ideas if args.skip_novelty_check else [idea for idea in ideas if idea["novel"]]
     # novel_ideas = list(reversed(novel_ideas))
 
     if args.parallel > 0:
